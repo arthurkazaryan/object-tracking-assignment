7a02ab9bfdf11aff2942af98723df0fdbffef038
arthurkazaryan <arthur.kazaryan@yahoo.com>
2023-05-04 03:15:08 +0300

    Переписан алгоритм софт трекера.

--- fastapi_server.py
+++ fastapi_server.py
@@ -12,14 +12,14 @@ imgs = glob.glob('imgs/*')
 country_balls = [{'cb_id': x, 'img': imgs[x % len(imgs)]} for x in range(country_balls_amount)]
 print('Started')
 
-TRACK_DEPTH = 3
+TRACK_DEPTH = 10
 
 
 def calculate_distance(current_ball_data: np.ndarray, mean_track_data: np.ndarray):
     return np.linalg.norm(current_ball_data - mean_track_data)
 
 
-def tracker_soft(el, tracking):
+def tracker_soft(el, tracking, coords):
     """
     Необходимо изменить у каждого словаря в списке значение поля 'track_id' так,
     чтобы как можно более длительный период времени 'track_id' соответствовал
@@ -35,28 +35,38 @@ def tracker_soft(el, tracking):
     - запрещается присваивать один и тот же track_id разным объектам на одном фрейме
     """
 
-    available_idx = [i for i in range(country_balls_amount)]# + [None]
-    print('------------new frame-------------')
+    def calculate_min_idx():
+        idx = None
+        min_distance = int(1e+3) + 9_000  # Да да мы приколисты
+        distance_list = []
+        for cb_id, deque_data in coords.items():
+            mean_track_data = np.array(deque_data).mean(axis=0)
+            distance = np.linalg.norm(current_ball_data - mean_track_data)
+            distance_list.append(distance)
+            if distance < min_distance:
+                min_distance = distance
+                idx = cb_id
+
+        probability = ((min_distance / distance_list) < 0.3).sum() / len(distance_list)
+        return idx, probability
+
     for ball_data in el['data']:
-        print('------------new ball-------------')
         if el['frame_id'] == 1:
             ball_data['track_id'] = ball_data['cb_id']
         else:
-            min_idx = None
-            min_distance = int(1e+3) + 9_000  # Да да мы приколисты
-            for cb_id, deque_data in tracking.items():
-                current_ball_data = np.array([ball_data['x'], ball_data['y']])
-                mean_track_data = np.array(deque_data).mean(axis=0)
-                distance = np.linalg.norm(current_ball_data - mean_track_data)
-                if distance < min_distance and cb_id in available_idx:
-                    min_distance = distance
-                    min_idx = cb_id
-                    print(ball_data["cb_id"], min_idx, available_idx)
-                    del available_idx[available_idx.index(cb_id)]
+            current_ball_data = np.array([ball_data['x'], ball_data['y']])
+            data = tracking[ball_data['cb_id']]
+            counts = np.bincount(data)
+            track_conf = counts[np.argmax(counts)] / len(data) * 0.8
 
-            print(ball_data["cb_id"], ball_data["track_id"])
+            m_idx, dist_conf = calculate_min_idx()
+            if track_conf >= dist_conf:
+                min_idx = np.argmax(counts)
+            else:
+                min_idx = m_idx
             ball_data["track_id"] = min_idx
-        tracking[ball_data['cb_id']].append([ball_data['x'], ball_data['y']])
+        tracking[ball_data['cb_id']].append(ball_data["track_id"])
+        coords[ball_data['cb_id']].append([ball_data['x'], ball_data['y']])
 
     return el, tracking
 
@@ -93,11 +103,12 @@ async def websocket_endpoint(websocket: WebSocket):
     # класса CountryBall на фронте
     await websocket.send_text(str(country_balls))
     tracking = {i: deque([], TRACK_DEPTH) for i in range(country_balls_amount)}
+    coords = {i: deque([], 3) for i in range(country_balls_amount)}
+
     for el in track_data:
         await asyncio.sleep(0.5)
         # TODO: part 1
-        el, tracking = tracker_soft(el, tracking)
-
+        el, tracking = tracker_soft(el, tracking, coords)
         # TODO: part 2
         # el = tracker_strong(el)
         # отправка информации по фрейму
